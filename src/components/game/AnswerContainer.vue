8f075ae03e1b4ac56504592f587ba953ea746166
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-10-04 14:43:38 +0200

    add guiz id to url

--- src/components/game/AnswerContainer.vue
+++ src/components/game/AnswerContainer.vue
@@ -14,7 +14,7 @@ getAnswers().then(obj => [
 ])
 
 async function getAnswers() {
-  const response = await fetch('http://localhost:8000/quiz');
+  const response = await fetch('http://localhost:8000/quiz/1');
   const data = await response.text();
   const obj = JSON.parse(data);
   console.log('Answers: ', obj);
