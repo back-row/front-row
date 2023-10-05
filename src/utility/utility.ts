96a9f018c62b91a8fca2613a7533d52b989b96b0
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-05 13:37:42 +0200

    update parse utility

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -2,6 +2,16 @@ export async function getAnswers() {
   const response = await fetch('http://localhost:8000/quiz/1');
   const data = await response.text();
   const obj = JSON.parse(data);
-  console.log('Answers: ', obj);
-  return obj;
+
+  const quizQuestion: {
+    question: string;
+    choice1: string;
+    answer1: string[];
+    choice2: string;
+    answer2: string[];
+    choice3: string;
+    answer3: string[];
+  } = JSON.parse(obj.quizquestion);
+
+  return quizQuestion;
 }
