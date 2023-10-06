c82732d3f886bd5e43739d59c0ed492dda6e1352
Julia Lerebäck Corell <112405931+jlereback@users.noreply.github.com>
2023-10-06 14:35:36 +0200

    Update src/components/game/MissionContainer.vue

--- src/components/game/MissionContainer.vue
+++ src/components/game/MissionContainer.vue
@@ -57,7 +57,7 @@ async function getTutorial(id: number) {
 
 <template>
   <div class="relative bg-[#2C3540] h-64 w-full mb-3 p-2 pt-10 rounded-sm">
-    <Button_ class="hover:animate-pulse" @click="showJson">
+    <Button class="hover:animate-pulse" @click="showJson">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -72,12 +72,12 @@ async function getTutorial(id: number) {
           d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
         />
       </svg>
-    </Button_>
+    </Button>
 
     <div v-if="isShowingHint" class=" absolute z-50 opacity-90 p-6">
       <div class=" bg-black  rounded-lg flex items-center -mt-20 mr-5 -ml-7  overflow-x-hidden overflow-y-auto ">
         <p class=" text-greenBackrow p-5 text-xl">
-          {{ currentMap.tutorialdescription }}
+          {{ currentMap.tutorialhint }}
 
         </p>
       </div>
