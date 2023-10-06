07191af07471f4ba3a0e5e9d94a51b161620154e
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-06 09:32:34 +0200

    fix custom component bug

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,4 @@
-<script setup lang='ts'>
-
+<script setup lang="ts">
 import { RouterLink } from 'vue-router';
 import Menu from 'primevue/menu';
 import { ref } from 'vue';
@@ -25,17 +24,35 @@ const items = ref([
 ]);
 
 const toggle = (event: any) => {
-    menu.value.toggle(event);
+  menu.value.toggle(event);
 };
 </script>
 
 <template>
   <nav class="flex items-center bg-[#0A0F14] text-white text-center h-10">
-    <Button class="ml-3" type="button" label="Toggle" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    <Button_
+      class="ml-3"
+      type="button"
+      label="Toggle"
+      @click="toggle"
+      aria-haspopup="true"
+      aria-controls="overlay_menu"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
       </svg>
-    </button>
+    </Button_>
     <Menu ref="menu" id="overlay_menu" :model="items" :popup="true">
       <template #item="{ label, item, props }">
         <router-link v-if="item.route" v-slot="routerProps" :to="item.route" custom>
@@ -51,5 +68,4 @@ const toggle = (event: any) => {
   </nav>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
