d8db6bf6573b308e3e0cf601c2204b00225eb9d4
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-06 14:49:00 +0200

    open and close modal on finish

--- src/components/game/WinModal.vue
+++ src/components/game/WinModal.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
+import { usePlayerStore } from '@/stores/player';
 import { RouterLink } from 'vue-router';
+
+const playerStore = usePlayerStore();
 </script>
 <template>
-  <div
-    class="z-20 absolute m-auto left-0 right-0 bg-[#0A0F14] w-4/5 h-1/2 flex flex-col justify-center items-center rounded-xl border-2 border-black shadow-black shadow-lg"
-  >
-    <img class="w-4/5 mt-4 rounded-lg" src="../../assets/finish.png" alt="finish" />
-    <router-link to="/">
-      <Button class="w-72 h-16 mt-4 rounded-lg text-2xl text-white bg-[#408080]"
-        >Back to home page
-      </Button></router-link
+  <div>
+    <div
+      class="z-20 absolute m-auto left-0 right-0 bg-[#0A0F14] w-4/5 flex flex-col justify-center items-center rounded-xl border-2 border-black shadow-black shadow-lg"
     >
+      <img
+        class="max-w-full max-h-full m-4 pt-10 px-20 rounded-lg"
+        src="../../assets/finish.png"
+        alt="finish"
+      />
+      <router-link to="/">
+        <Button
+          @onclick="playerStore.playerPosition.atEnd = false"
+          class="w-72 h-16 mb-4 rounded-lg text-2xl text-white bg-[#408080]"
+          >Back to home page
+        </Button></router-link
+      >
+    </div>
+    <div class="absolute m-auto left-0 right-0 w-screen h-screen z-10 bg-black opacity-50"></div>
   </div>
-  <div class="fixed w-full h-full z-10 bg-black opacity-50"></div>
 </template>
 <style scoped></style>
