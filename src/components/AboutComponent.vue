27ffd27bccebf533a533c274c897a3b5dbff57ea
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-10-09 10:21:32 +0200

    Add text area

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -3,7 +3,19 @@
 </script>
 
 <template>
-  <header class="text-center text-lg text-[#E5E5E5] mt-10">About Us</header>
+  <div class="flex flex-col items-center">
+    <header class="text-center text-6xl text-[#E5E5E5] m-10">About Us</header>
+      <div class="flex container bg-[#E5E5E5] w-3/5 rounded-2xl">
+        <p class="text-center p-12">
+          In Back Row, we've turned learning to code into a thrilling adventure! 
+          Here we've transformed learning to code into an exhilarating adventure! 
+          Whether you're a beginner or an aspiring coder, get ready to delve into the 
+          world of programming. You'll lead your character on an epic quest brimming with 
+          excitement and discovery. This game is thoughtfully designed to make 
+          programming enjoyable and accessible for beginners of all ages, ensuring that you 
+          have a great time while learning.</p>
+      </div>  
+  </div>   
 </template>
 
 <style>
