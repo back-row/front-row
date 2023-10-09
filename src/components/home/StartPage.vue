bb039a3fa49ff6991b080c402aeb1b8abbe5cfd3
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-09 15:21:15 +0200

    Add animation images

--- src/components/home/StartPage.vue
+++ src/components/home/StartPage.vue
@@ -5,20 +5,20 @@ import { RouterLink } from 'vue-router';
 <template>
   <div class="start grid grid-cols-2 grid-rows-2 gap-x-3 gap-y-3 mt-4">
     <div class="knowledge col-span-1 col-start-1 row-span-1 row-start-1 flex items-center justify-center">
-      <h2 class="rounded-full ml-12 -rotate-12 h-24 w-96 bg-[#2C3540] flex items-center justify-center text-4xl text-[#E5E5E5]" >Spela dig till kunskap</h2>
+      <h2 class="rounded-full ml-12 -rotate-12 h-24 w-96 bg-[#2C3540] flex items-center justify-center text-4xl text-[#E5E5E5]">Spela dig till kunskap</h2>
     </div>
     <div class="logo col-span-1 row-span-1 row-start-1 col-start-2 flex items-center justify-center">
       <img src="src\assets\logo-nobg.png">
     </div>
     <div class="characters col-span-1 col-start-1 row-span-1 row-start-2">
       <div class="flex flex-nowrap rounded-xl ml-24 mr-12 p-6 bg-[#0A0F14]">
-        <div class="character">
+        <div class="character ease-in-out duration-300 hover:scale-110">
           <img src="src\assets\bluePlayer_nobg.png">
         </div>
-        <div class="character">
+        <div class="character ease-in-out duration-300 hover:scale-110">
           <img src="src\assets\yellowPlayer_nobg.png">
         </div>
-        <div class="character">
+        <div class="character ease-in-out duration-300 hover:scale-110">
           <img src="src\assets\greenPlayer_nobg.png">
         </div>
      </div>
