67c557ea68d896cff39cb528d3c52c5370d2de3f
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-09 15:51:21 +0200

    move player with AnswerContainer

--- src/stores/player.ts
+++ src/stores/player.ts
@@ -21,7 +21,6 @@ export const usePlayerStore = defineStore('player', () => {
     mapId: 1,
     atEnd: false
   });
-
   const moveUp = async () => {
     if (playerPosition.value.player) playerPosition.value.player.setVelocityY(-200);
   };
