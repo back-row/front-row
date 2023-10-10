15236f9a55f6245d6f25d6b7609f5de1505a954a
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-10-10 11:08:29 +0200

    Add LevelComponent

---
+++ src/components/level/LevelComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <div class="bg-[#E5E5E5] w-1/2 mt-20 rounded-lg">
+        </div>
+    </div>    
+</template>
+<style scoped>
+</style>
