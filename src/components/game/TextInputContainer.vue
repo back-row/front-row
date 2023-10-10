78cd2f8178b7855d3da6b3cff5230bd8dac75313
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-10 11:07:07 +0200

    Add TextInputContainer

---
+++ src/components/game/TextInputContainer.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import type { Ref } from 'vue';
+import { getAnswers } from '@/utility/utility';
+
+type answer = { choice: string; answer: string[] };
+const selectedAnswer = ref([]);
+const userInput = ref('');
+const question = ref('');
+
+onMounted(() => {
+  getAnswers().then((obj) => [
+
+    (question.value = obj.question)
+  ]);
+});
+
+</script>
+
+<template>  
+  <div class="relative bg-[#2C3540] h-80 w-full p-2 pt-4 rounded-sm">
+    <div class="bg-[#E5E5E5] h-4/5">
+      <div class="question">{{ question }}</div>
+      <div v-bind="userInput">
+        <textarea class="ml-4 mt-4" id="" cols="45" rows="5" placeholder="Type your code here..."></textarea>
+      </div>
+    </div>
+    <button
+      type="submit"
+      @click.prevent=""
+      class="hover:animate-pulse bg-[#408080] h-10 w-20 m-2 rounded-md flex items-center justify-center absolute bottom-0 right-0 text-white"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+        />
+      </svg>
+      Run
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -2,6 +2,7 @@
 import GameContainer from '@/components/game/GameContainer.vue';
 import MissionContainer from '@/components/game/MissionContainer.vue';
 import AnswerContainer from '@/components/game/AnswerContainer.vue';
+import TextInputContainer from '@/components/game/TextInputContainer.vue';
 import WinModal from '@/components/game/WinModal.vue';
 import { usePlayerStore } from '@/stores/player';
 
@@ -15,7 +16,8 @@ const playerStore = usePlayerStore();
     </div>
     <div class="sideArea">
       <MissionContainer />
-      <AnswerContainer />
+      <!-- <AnswerContainer /> --> 
+      <TextInputContainer />
     </div>
     <div v-show="playerStore.playerPosition.atEnd">
       <WinModal />
