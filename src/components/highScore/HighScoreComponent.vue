c1ccf77ebe6b625b70b8d7b21ba73b20036b8d87
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-10-10 08:52:40 +0200

    update list

--- src/components/highScore/HighScoreComponent.vue
+++ src/components/highScore/HighScoreComponent.vue
@@ -22,7 +22,7 @@ const highScore: scores[] = [
   <div class="flex flex-col items-center">
     <div class="bg-[#E5E5E5] w-7/12 mt-20 rounded-lg">
       <h1 class="text-center text-3xl mt-12 mb-6">HIGH SCORE</h1>
-      <ol class="list-decimal list-inside text-lg ml-32 mr-32 mb-12 overflow-hidden" >
+      <ol class="list-decimal list-inside text-lg ml-32 mr-32 mb-12" >
         <li v-for="score in highScore" :key="score.name">
           <span class="pl-3">{{ score.name }}</span>
           <span class="float-right">{{ score.score }}</span>
