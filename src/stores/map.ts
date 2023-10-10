dd3d45f7893387cfdebb9fdd29073313e8793c49
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-10 11:04:42 +0200

    update MissionContainer to use map store

--- src/stores/map.ts
+++ src/stores/map.ts
@@ -3,11 +3,11 @@ import { ref } from 'vue';
 
 export const useMapStore = defineStore('map', () => {
   const map = ref({
-    Id: 1,
-    IsQuiz: false,
-    QuizId: 0,
-    EndLocationX: 0,
-    EndLocationY: 0,
+    id: 1,
+    isQuiz: false,
+    quizId: 0,
+    endLocationX: 0,
+    endLocationY: 0,
     tutorialId: 0,
     atEnd: false
   });
