d58f14db4e64fb5074f54f1bee69efd5289cbbcf
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-10-10 11:13:35 +0200

    Add LevelView

---
+++ src/views/LevelView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LevelComponent from '@/components/level/LevelComponent.vue';
+</script>
+
+<template>
+  <LevelComponent />
+</template>
+
+<style scoped>
+
+</style>
