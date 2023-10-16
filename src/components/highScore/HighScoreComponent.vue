b28fef6750a4af15153e15fdaf01aa3b4726b060
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-16 11:48:04 +0200

    add fetch from db to highscorelist

--- src/components/highScore/HighScoreComponent.vue
+++ src/components/highScore/HighScoreComponent.vue
@@ -2,23 +2,17 @@
 import { RouterLink } from 'vue-router';
 import { useMapStore } from '@/stores/map';
 import { useUserStore } from '@/stores/user';
+import { getHighScore } from '@/utility/utility';
+import { onMounted, ref, type Ref } from 'vue';
 
-type scores = { name: string; score: string };
+type scores = { usersname: string; userstotalscore: number };
 const userStore = useUserStore();
 const mapStore = useMapStore();
+const highScore: Ref<scores[]> = ref([]);
 
-const highScore: scores[] = [
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' },
-  { name: 'test test', score: '2000' }
-];
+onMounted(async () => {
+  highScore.value = await getHighScore();
+});
 </script>
 
 <template>
@@ -26,14 +20,17 @@ const highScore: scores[] = [
     <div class="bg-whiteBackRow w-1/2 mt-20 rounded-lg text-blackBackRow">
       <h1 class="text-center text-3xl mt-12 mb-6">HIGH SCORE</h1>
       <ol class="list-decimal list-inside text-lg ml-32 mr-32 mb-12">
-        <li v-for="score in highScore" :key="score.name">
-          <span class="pl-3">{{ score.name }}</span>
-          <span class="float-right">{{ score.score }}</span>
+        <li v-for="score in highScore" :key="score.usersname">
+          <span class="pl-3">{{ score.usersname }}</span>
+          <span class="float-right">{{ score.userstotalscore }}</span>
         </li>
       </ol>
     </div>
     <router-link to="/game">
-      <button @click="mapStore.getMapFromDb(userStore.user.level)" class="w-64 h-16 m-4 rounded-lg text-2xl text-white bg-[#408080] hover:animate-pulse">
+      <button
+        @click="mapStore.getMapFromDb(userStore.user.level)"
+        class="w-64 h-16 m-4 rounded-lg text-2xl text-white bg-[#408080] hover:animate-pulse"
+      >
         Play
       </button>
     </router-link>
