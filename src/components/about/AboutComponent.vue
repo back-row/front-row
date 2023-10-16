0df4b940a1b66fe7dc82dc3f8fe7f1f32d771931
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-16 12:08:19 +0200

    Add media queries to about page

--- src/components/about/AboutComponent.vue
+++ src/components/about/AboutComponent.vue
@@ -2,9 +2,9 @@
 
 <template>
   <div class="flex flex-col items-center">
-    <header class="text-center text-7xl text-whiteBackRow m-8">About Us</header>
-    <div class="flex container bg-whiteBackRow w-4/5 rounded-2xl">
-      <p class="text-center text-blackBackRow text-xl p-24 pt-10 pb-10">
+    <header class="text-3xl text-center md:text-7xl text-[#E5E5E5] m-8">About Us</header>
+    <div class="flex container bg-[#E5E5E5] w-4/5 rounded-2xl">
+      <p class="p-4 text-center md:text-xl md:p-24 md:py-10">
         In Back Row, we've turned learning to code into a thrilling adventure! Here we've
         transformed learning to code into an exhilarating adventure! Whether you're a beginner or an
         aspiring coder, get ready to delve into the world of programming. You'll lead your character
@@ -13,24 +13,24 @@
         have a great time while learning.
       </p>
     </div>
-    <div class="flex justify-evenly m-10 w-4/5 text-blackBackRow">
-      <div class="flex flex-col ">
+    <div class="flex flex-wrap justify-evenly m-10 w-4/5">
+      <div>
         <img class="h-32 w-32 rounded-full" src="../../assets/about/head1.png" alt="blue" />
         <p class="text-center bg-whiteBackRow p-1 rounded-md mt-4">Oliver</p>
       </div>
-      <div class="flex flex-col">
+      <div>
         <img class="h-32 w-32 rounded-full" src="../../assets/about/head2.png" alt="blue" />
         <p class="text-center bg-whiteBackRow p-1 rounded-md mt-4">Robert</p>
       </div>
-      <div class="flex flex-col">
+      <div>
         <img class="h-32 w-32 rounded-full" src="../../assets/about/head3.png" alt="blue" />
         <p class="text-center bg-whiteBackRow p-1 rounded-md mt-4">Fredrik</p>
       </div>
-      <div class="flex flex-col">
+      <div>
         <img class="h-32 w-32 rounded-full" src="../../assets/about/head4.png" alt="blue" />
         <p class="text-center bg-whiteBackRow p-1 rounded-md mt-4">Julia</p>
       </div>
-      <div class="flex flex-col">
+      <div>
         <img class="h-32 w-32 rounded-full" src="../../assets/about/head5.png" alt="blue" />
         <p class="text-center bg-whiteBackRow p-1 rounded-md mt-4">Henrik</p>
       </div>
