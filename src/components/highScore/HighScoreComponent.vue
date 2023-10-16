013be5000dc0fc52a76858f4ca29dcc4f203ee96
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-16 14:11:57 +0200

    Add mq to highScore

--- src/components/highScore/HighScoreComponent.vue
+++ src/components/highScore/HighScoreComponent.vue
@@ -17,10 +17,10 @@ onMounted(async () => {
 
 <template>
   <div class="flex flex-col items-center">
-    <div class="bg-whiteBackRow w-1/2 mt-20 rounded-lg text-blackBackRow">
-      <h1 class="text-center text-3xl mt-12 mb-6">HIGH SCORE</h1>
-      <ol class="list-decimal list-inside text-lg ml-32 mr-32 mb-12">
-        <li v-for="score in highScore" :key="score.usersname">
+    <div class="bg-whiteBackRow md:w-1/2 mt-20 rounded-lg text-blackBackRow">
+      <h1 class="text-center sm:text-3xl mt-12 mb-6">HIGH SCORE</h1>
+      <ol class="list-decimal list-inside mx-12 md:text-lg md:ml-32 md:mr-32 mb-12">
+        <li v-for="score in highScore" :key="score.usersname" class="odd:bg-gray-300">
           <span class="pl-3">{{ score.usersname }}</span>
           <span class="float-right">{{ score.userstotalscore }}</span>
         </li>
