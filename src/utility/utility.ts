0e9e5763d7b9a7ea001cb4deaed0429b0c89833f
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-18 14:56:43 +0200

    fix quiz bug

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -1,21 +1,11 @@
 export async function getAnswers(id: number) {
   const response = await fetch('http://localhost:8000/quiz/' + id);
-  const data = await response.text();
-  const obj = JSON.parse(data);
+  const data = await response.json();
 
-  const quizQuestion: {
-    question: string;
-    choice1: string;
-    answer1: string[];
-    choice2: string;
-    answer2: string[];
-    choice3: string;
-    answer3: string[];
-  } = JSON.parse(obj.quizquestion);
+  const quizQuestion = JSON.parse(data.quizquestion);
 
   return quizQuestion;
 }
-
 export async function getNumberOfMaps() {
   const response = await fetch('http://localhost:8000/map/count');
   const data = await response.text();
