86c6efcd7ac56ddcf9d9a274afe913b43fc884eb
RobertMili <robertmilicevic3869@gmail.com>
2023-10-23 13:32:22 +0200

    add router link to next map

--- src/components/game/NextMap.vue
+++ src/components/game/NextMap.vue
@@ -8,11 +8,6 @@ const mapStore = useMapStore();
 const showModal = ref(true);
 
 
-const startGame = async (map: number) => {
-  showModal.value = false;
-   await mapStore.getMapFromDb(2);
- 
-};
 </script>
 <template>
   <div v-if="showModal" >
@@ -24,20 +19,13 @@ const startGame = async (map: number) => {
     src="../../assets/nextLvl.jpg"
     alt="finish"
     />
-    
+    <router-link to="/level">
     <button
-        
-        @click="startGame(2)"
-        
         class="w-48 h-16 flex items-center justify-center absolute top-28 right-36 rounded-lg text-2xl text-whiteBackRow bg-greenBackrow"
         >
-        Next level
-  </button>
- 
-     
-   
-   
-     
+          Next level
+    </button>
+    </router-link>
     </div>
     <div  class="absolute m-auto left-0 right-0 w-screen h-screen z-10 bg-blackBackrow opacity-50">
 
