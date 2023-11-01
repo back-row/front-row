ff3128d7df63b57ae2e91d3a231cb7451f661777
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-11-01 09:23:43 +0100

    Add translations to sideContainer

--- src/components/game/SideContainer.vue
+++ src/components/game/SideContainer.vue
@@ -16,8 +16,8 @@ const changeDifficulty = () => {
 <template>
   <div class="sideArea sm:hidden">
     <div class="flex text-blackBackRow dark:text-greenBackRow w-fit mt-4">
-      <button class="p-2 cursor-pointer mx-2 dark:bg-grayLightBackRow border border-b-0 shadow-lg border-grayLightBackRow dark:border-none rounded-t-md" @click="activeButton = false" :class="{'text-2xl font-bold': activeButton === false}">Mission</button>
-      <button class="p-2 cursor-pointer dark:bg-grayLightBackRow border border-b-0 border-grayLightBackRow dark:border-none rounded-t-md" @click="activeButton = true" :class="{'text-2xl font-bold': activeButton === true}">Answer</button>
+      <button class="p-2 cursor-pointer mx-2 dark:bg-grayLightBackRow border border-b-0 shadow-lg border-grayLightBackRow dark:border-none rounded-t-md" @click="activeButton = false" :class="{'text-2xl font-bold': activeButton === false}">{{$t('mission')}}</button>
+      <button class="p-2 cursor-pointer dark:bg-grayLightBackRow border border-b-0 border-grayLightBackRow dark:border-none rounded-t-md" @click="activeButton = true" :class="{'text-2xl font-bold': activeButton === true}">{{$t('answer')}}</button>
     </div>
     <MissionContainer v-show="!activeButton"/>
     <div v-show="activeButton">
